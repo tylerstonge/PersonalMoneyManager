51b2e9b6291531c4079b3b806a55afa9f6031524
Tyler St. Onge <tylertstonge@gmail.com>
2016-03-09 01:30:22 -0500

    generated project

---
+++ src/androidTest/java/edu/oswego/tygama344/MainActivityTest.java
@@ -0,0 +1,21 @@
+package edu.oswego.tygama344;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class edu.oswego.tygama344.MainActivityTest \
+ * edu.oswego.tygama344.tests/android.test.InstrumentationTestRunner
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super("edu.oswego.tygama344", MainActivity.class);
+    }
+
+}
