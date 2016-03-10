dabc0f545a7df93708e7f1085d65b0c8e52a4333
Tyler St. Onge <tylertstonge@gmail.com>
2016-03-10 12:46:12 -0500

    started tests on MainActivity

--- src/androidTest/java/edu/oswego/tygama344/MainActivityTest.java
+++ src/androidTest/java/edu/oswego/tygama344/MainActivityTest.java
@@ -1,6 +1,12 @@
 package edu.oswego.tygama344;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.database.DataSetObserver;
+import android.widget.ListView;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.UiThreadTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * This is a simple framework for a test of an Application.  See
@@ -12,10 +18,28 @@ import android.test.ActivityInstrumentationTestCase2;
  * -e class edu.oswego.tygama344.MainActivityTest \
  * edu.oswego.tygama344.tests/android.test.InstrumentationTestRunner
  */
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     public MainActivityTest() {
         super("edu.oswego.tygama344", MainActivity.class);
     }
 
+    @Test
+    public void testActivityExists() {
+	MainActivity mainActivity = getActivity();
+	assertNotNull(mainActivity);
+    }
+
+    @UiThreadTest
+    public void testPopulateList() {
+	final ListView lv = (ListView) getActivity().findViewById(R.id.historyListView);
+	lv.getAdapter().registerDataSetObserver(new DataSetObserver() {
+		@Override
+		public void onChanged() {
+			assertTrue(lv.getCount() > 0);
+		}
+	});
+	getActivity().populateListView(new String[] { "Gas", "Groceries" });
+    } 
 }
--- src/main/java/edu/oswego/tygama344/MainActivity.java
+++ src/main/java/edu/oswego/tygama344/MainActivity.java
@@ -2,14 +2,36 @@ package edu.oswego.tygama344;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity
-{
+
+public class MainActivity extends Activity {
+    
+    ListView lv;	
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+	// Get a handle to the ListView 
+	lv = (ListView) findViewById(R.id.historyListView);
+
+	// Dummy values to populate listview
+	String[] items = new String[] { "Gas", "Power", "Groceries" };
+	populateListView(items);
+    }
+
+    /**
+     *	Sets the information the ListView will display
+     *
+     *	@param items the String[] of items to display
+     */
+    public void populateListView(String[] items) {
+	ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, items);
+	lv.setAdapter(adapter);
     }
 }
