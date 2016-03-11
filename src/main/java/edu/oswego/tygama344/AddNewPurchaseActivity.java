2b6010c1bea0f417caffb592d5851be1b1c04fc3
Max Sokolovsky <msokolov@oswego.edu>
2016-03-10 23:07:24 -0500

    button logic

---
+++ src/main/java/edu/oswego/tygama344/AddNewPurchaseActivity.java
@@ -0,0 +1,12 @@
+package edu.oswego.tygama344;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddNewPurchaseActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
