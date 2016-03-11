e11e5c1d412abb101e3b15e5262a863c5c4bafdd
Gaoping Lin <glin2@oswego.edu>
2016-03-10 23:34:23 -0500

    added cancel and submit button test

--- src/androidTest/java/edu/oswego/tygama344/AddNewPurchaseActivityTest.java
+++ src/androidTest/java/edu/oswego/tygama344/AddNewPurchaseActivityTest.java
@@ -2,17 +2,39 @@ package edu.oswego.tygama344;
 
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.widget.Button;
 
+import junit.framework.TestResult;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * Created by leo on 3/10/16.
  */
 @RunWith(AndroidJUnit4.class)
-public class AddNewPurchaseActivityTest  extends ActivityInstrumentationTestCase2<MainActivity> {
+public class AddNewPurchaseActivityTest  extends ActivityInstrumentationTestCase2<AddNewPurchaseActivity> {
 
 
-    public AddNewPurchaseActivityTest(Class<MainActivity> activityClass) {
-        super(AddNewPurchaseActivity.class);
+    public AddNewPurchaseActivityTest() {
+        super("edu.oswego.tygama344", AddNewPurchaseActivity.class);
+    }
+
+    @Test
+    public void testActivityExists(){
+        AddNewPurchaseActivity purchaseActivity = getActivity();
+        assertNotNull(purchaseActivity);
+    }
+
+    @UiThreadTest
+    public void testViewContainsCancelButton(){
+        final Button cancelButton = (Button) getActivity().findViewById(R.id.cancelButton);
+        assertNotNull(cancelButton);
+    }
+
+    @UiThreadTest
+    public void testViewContainsSubmitButton(){
+        final Button submitButton = (Button) getActivity().findViewById(R.id.submitButton);
+        assertNotNull(submitButton);
     }
 }
