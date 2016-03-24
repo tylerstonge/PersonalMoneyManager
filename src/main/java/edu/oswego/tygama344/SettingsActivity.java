97913a18fd04f1f646ad043f27aa9e5137644261
telegrammae <telegrammae@gmail.com>
2016-03-24 14:56:13 -0400

    switch

--- src/main/java/edu/oswego/tygama344/SettingsActivity.java
+++ src/main/java/edu/oswego/tygama344/SettingsActivity.java
@@ -1,13 +1,59 @@
 package edu.oswego.tygama344;
 
-import android.app.Activity;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+import android.widget.TextView;
 
 
 public class SettingsActivity extends Activity {
+    private TextView switchStatus;
+    private Switch mySwitch;
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        switchStatus = (TextView) findViewById(R.id.switchStatus);
+        mySwitch = (Switch) findViewById(R.id.stats);
+
+        //set the switch to ON
+        mySwitch.setChecked(true);
+        //attach a listener to check for changes in state
+        mySwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+
+                if (isChecked) {
+                    switchStatus.setText("Switch is currently ON");
+                } else {
+                    switchStatus.setText("Switch is currently OFF");
+                }
+
+            }
+        });
+
+        //check the current state before we display the screen
+        if (mySwitch.isChecked()) {
+            switchStatus.setText("Switch is currently ON");
+        } else {
+            switchStatus.setText("Switch is currently OFF");
+        }
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.settings, menu);
+//        return true;
+//    }
 }
