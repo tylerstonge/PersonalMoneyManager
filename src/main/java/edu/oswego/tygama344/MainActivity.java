b7185599739adead85705026a2c0132a37d186ec
Tyler St. Onge <tylertstonge@gmail.com>
2016-05-03 19:26:54 -0400

    scrolling graphs. less views. more awesome.

--- src/main/java/edu/oswego/tygama344/MainActivity.java
+++ src/main/java/edu/oswego/tygama344/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.PieChart;
@@ -57,9 +56,7 @@ public class MainActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        RelativeLayout chart = (RelativeLayout) findViewById(R.id.rel);
-        mChart = new PieChart(this);
-        chart.addView(mChart, 1200, 1200);
+        mChart = (PieChart) findViewById(R.id.rel);
         //chart.setBackgroundColor(Color.parseColor("#000000"));
         mChart.setUsePercentValues(true);
 
